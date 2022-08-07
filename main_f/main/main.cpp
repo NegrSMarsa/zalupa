5ecb05513179b7b744acb54044b056ada4607b58
NegrSMarsa <110737988+NegrSMarsa@users.noreply.github.com>
2022-08-07 11:19:03 +0500

    Add files via upload

---
+++ main_f/main/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int a, b, c, summa, proiz, sred;
+
+	cout << "Введите 3 числа: \n";
+	cin >> a >> b >> c;
+
+	cout << "\n\n";
+
+	cout << "Вы ввели: " << a << ", " << b << ", " << c << "\n";
+	summa = a + b + c;
+	cout << "Сумма чисел равна: " << summa<<"\n";
+	proiz = a * b * c;
+	cout << "Произведение чисел равно: " << proiz<<'\n';
+	sred = (a + b + c) / 3;
+	cout << "Среднее Арифметическое чисел: " << sred<<"\n\n";
+}	
